ee27cd6c2e8697734f2525b1772a7de571cfedf5
caphey <99432180+caphey@users.noreply.github.com>
2023-03-23 21:06:35 +0100

    add gradient class

--- landingPageBenAmor/tailwind.config.cjs
+++ landingPageBenAmor/tailwind.config.cjs
@@ -2,7 +2,24 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#000000",
+        secondary: "#6D95FC",
+        contrast: "#FFFFFF",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
   },
   plugins: [],
-}
+};
