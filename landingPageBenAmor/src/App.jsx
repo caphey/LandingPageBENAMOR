c3853e440ef91566cddd90a62350346ef5637fa9
caphey <99432180+caphey@users.noreply.github.com>
2023-03-24 15:48:18 +0100

    Update setup
    Add constants, add index.css, add components

--- landingPageBenAmor/src/App.jsx
+++ landingPageBenAmor/src/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import styles from "./style";
+import { Navbar, Hero } from "./components";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      {/* <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div> */}
+
+      {/* <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+        autre parties
+        </div>
+      </div> */}
+    </div>
+  );
 };
 
 export default App;
---
+++ landingPageBenAmor/src/constants/index.js
@@ -0,0 +1,143 @@
+import {
+  facebook,
+  instagram,
+  twitter,
+  pinterest,
+  whatapp,
+  linkedin,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "about",
+    title: "About Us",
+  },
+  {
+    id: "services",
+    title: "Services",
+  },
+  {
+    id: "portfolio",
+    title: "Portfolio",
+  },
+  {
+    id: "pages",
+    title: "Pages",
+  },
+  {
+    id: "contact",
+    title: "Contact Us",
+  },
+  {
+    id: "search",
+    // icon:
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    // icon: ,
+    title: "Video Marketing",
+    content: "Lorem ipsum Neque do porro quisquam est qui do quam ",
+  },
+  {
+    id: "feature-2",
+    // icon: ,
+    title: "Graphi Design",
+    content: "Lorem ipsum Neque do porro quisquam est qui do quam ",
+  },
+  {
+    id: "feature-3",
+    // icon ,
+    title: "UI/UX Design",
+    content: "Lorem ipsum Neque do porro quisquam est qui do quam ",
+  },
+  {
+    id: "feature-4",
+    // icon ,
+    title: "Website Design",
+    content: "Lorem ipsum Neque do porro quisquam est qui do quam ",
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Team members",
+    value: "50+",
+  },
+  {
+    id: "stats-2",
+    title: "Happy ",
+    value: "200+",
+  },
+  {
+    id: "stats-3",
+    title: "Happy Customers",
+    value: "500+",
+  },
+  {
+    id: "stats-4",
+    title: "Award Winning",
+    value: "75+",
+  },
+];
+
+export const footerLinks = [
+  {
+    id: "categories",
+    title: "Categories",
+  },
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "services",
+    title: "Services",
+  },
+  {
+    id: "portfolio",
+    title: "Portfolio",
+  },
+  {
+    id: "pages",
+    title: "Pages",
+  },
+  {
+    id: "support",
+    title: "Support",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "facebook",
+    icon: facebook,
+  },
+  {
+    id: "instagram",
+    icon: instagram,
+  },
+  {
+    id: "whatsapp",
+    icon: whatapp,
+  },
+  {
+    id: "linkedin",
+    icon: linkedin,
+  },
+  {
+    id: "pinterest",
+    icon: pinterest,
+  },
+  {
+    id: "twitter",
+    icon: twitter,
+  },
+];
